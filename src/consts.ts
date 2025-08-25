5d3daaff6122ecf2b0e1184426d48d5c591126ac
Michael Kumm <michael@mkumm.com>
2025-08-25 11:30:46 +0200

    started adding content

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Michael Kumm';
+export const SITE_DESCRIPTION = 'I am a software developer focused on functional languages - most of my code is Elixir';
